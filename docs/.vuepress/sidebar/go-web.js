ad0d8097364bad52b25b58de91acdabf778ac9f7
elgar17 <1653794708@qq.com>
2021-12-03 21:19:19 +0800

    更新协议

--- docs/.vuepress/sidebar/go-web.js
+++ docs/.vuepress/sidebar/go-web.js
@@ -2,12 +2,11 @@ module.exports = [{
     title: 'Go 网络编程',
     children: [
         '',
-        '1-hello.md',
-        '2-request.md',
-        '3-responce.md',
-        '4-session.md',
-        '5-sql.md',
-        '6-websocket.md'
-
+        '1-hello',
+        '2-request',
+        '3-responce',
+        '4-session',
+        '5-sql',
+        '6-websocket'
     ],
 }]
